9a950fdcccc34884c721d9c4510b760b3ea305e9
NQAnh01 <ngquocanh280101@gmail.com>
2024-07-26 21:06:58 +0700

    update services renderer

--- src/app/Config/Routes.php
+++ src/app/Config/Routes.php
@@ -9,8 +9,6 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'LoginController::index');
 $routes->get('/login', 'LoginController::index');
 $routes->get('about', 'AboutController::index');
-// $routes->get('admin/dashboard', 'Madmin\DashboardController::index');
-// $routes->get('admin/login', 'Madmin\AuthController::index');
 $routes->group('admin', static function ($routes) {
     $routes->get('dashboard', 'Madmin\DashboardController::index');
     $routes->get('login', 'Madmin\AuthController::index');
--- src/app/Controllers/Madmin/DashboardController.php
+++ src/app/Controllers/Madmin/DashboardController.php
@@ -3,21 +3,14 @@
 namespace App\Controllers\Madmin;
 
 use App\Controllers\MadminController;
+use CodeIgniter\Config\Services;
 
 class DashboardController extends MadminController
 {
     public function index()
     {
-        $data = [
-            'title' => 'Dashboard v2',
-        ];
-
-        echo $this->loadView('commons/admin/inc/dashboard/content', $data);
-    }
-
-    public function loadView($view, $data = [])
-    {
-        $data['content'] = view($view, $data);
+        $data['title'] = 'AdminLTE 3 |Dashboard v2';
+        $data['content'] = Services::renderer()->render('commons/admin/inc/dashboard/content');
         return view('commons/admin/pages/dashboard/index', $data);
     }
 }
