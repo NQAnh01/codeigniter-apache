492d3bdf70cc08f0ace8029fb7ef391824bfb47c
NQAnh01 <ngquocanh280101@gmail.com>
2024-07-26 16:05:58 +0700

    Add render method

--- src/app/Views/commons/admin/pages/dashboard/index.php
+++ src/app/Views/commons/admin/pages/dashboard/index.php
@@ -1,34 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php echo $this->extend('commons/admin/pages/dashboard/layout') ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | <?php echo esc($title) ?></title>
+<?php echo $this->section('title') ?>
+    <?php echo esc($title) ?>
+<?php echo $this->endSection() ?>
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome Icons -->
-    <link rel="stylesheet" href="<?php echo base_url('plugins/fontawesome-free/css/all.min.css') ?>">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?php echo base_url('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo base_url('dist/css/adminlte.min.css') ?>">
-</head>
-
-<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-    <div class="wrapper">
-        <?php echo $this->include('commons/admin/inc/header') ?>
-        <?php echo $this->include('commons/admin/inc/sidebar') ?>
-        <div class="content-wrapper">
-            <?php echo $this->include('commons/admin/inc/breadcrumb') ?>
-            <?php echo $content ?>
-        </div>
-        <?php echo $this->include('commons/admin/inc/footer') ?>
-    </div>
-
-    <!-- REQUIRED SCRIPTS -->
-    <?php echo $this->include('commons/admin/inc/dashboard/script') ?>
-</body>
-
-</html>
+<?php echo $this->section('content') ?>
+    <?php echo $content ?>
+<?php echo $this->endSection() ?>
---
+++ src/app/Views/commons/admin/pages/dashboard/layout.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | <?php echo $this->renderSection('title') ?></title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="<?php echo base_url('plugins/fontawesome-free/css/all.min.css') ?>">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="<?php echo base_url('plugins/overlayScrollbars/css/OverlayScrollbars.min.css') ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?php echo base_url('dist/css/adminlte.min.css') ?>">
+</head>
+
+<body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <?php echo $this->include('commons/admin/inc/header') ?>
+        <?php echo $this->include('commons/admin/inc/sidebar') ?>
+        <div class="content-wrapper">
+            <?php echo $this->include('commons/admin/inc/breadcrumb') ?>
+            <?php echo $this->renderSection('content') ?>
+        </div>
+        <?php echo $this->include('commons/admin/inc/footer') ?>
+    </div>
+
+    <!-- REQUIRED SCRIPTS -->
+    <?php echo $this->include('commons/admin/inc/dashboard/script') ?>
+</body>
+
+</html>
